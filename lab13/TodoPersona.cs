71a4a0af51b5e0a66a99539c29ddadfd83821a91
soulone <ascf24032@gmail.com>
2019-11-18 19:33:03 -0500

    First Commit

---
+++ lab13/TodoPersona.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using SQLite;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace lab13
+{
+    public class TodoPersona
+    {
+
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string dni { get;set }
+        public string nombre { get; set; }
+        public string apellidoPaterno { get; set; }
+        public string apellidoMaterno { get; set; }
+        public string fechaNacimiento { get; set; }
+
+
+    }
+}
---
+++ lab13/todoPersonaBaseDatos.cs
@@ -0,0 +1,54 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace lab13
+{
+    public class todoPersonaBaseDatos
+    {
+        readonly SQLiteAsyncConnection database;
+
+        public todoPersonaBaseDatos(string dbPath)
+        {
+            database = new SQLiteAsyncConnection(dbPath);
+            database.CreateTableAsync<TodoPersona>().Wait();
+        }
+
+        public Task<List<TodoPersona>> GetItemsAsync()
+        {
+            return database.Table<TodoPersona>().ToListAsync();
+        }
+
+        public Task<List<TodoPersona>> GetItemsNotDoneAsync()
+        {
+            return database.QueryAsync<TodoPersona>("SELECT * FROM [TodoItem] WHERE [Done] = 0");
+        }
+
+        public Task<TodoPersona> GetItemAsync(int id)
+        {
+            return database.Table<TodoPersona>().Where(i => i.ID == id).FirstOrDefaultAsync();
+        }
+
+        public Task<int> SaveItemAsync(TodoPersona item)
+        {
+            if (item.ID != 0)
+            {
+                return database.UpdateAsync(item);
+            }
+            else
+            {
+                return database.InsertAsync(item);
+            }
+        }
+
+        public Task<int> DeleteItemAsync(TodoPersona item)
+        {
+            return database.DeleteAsync(item);
+        }
+    }
+}
+
+
